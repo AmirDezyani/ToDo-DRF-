7f40b7f80c56a6de8c658eea704693735131095b
amiru <you.nic.sho@gmail.com>
2025-06-24 16:25:47 +0330

    v 0.2

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
-    'public'
+    'public',
+    'accounts_app',
 ]
 
 MIDDLEWARE = [
--- ToDo/urls.py
+++ ToDo/urls.py
@@ -18,5 +18,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todos/', include('public.urls')),
+    path('accounts/', include('accounts_app.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
